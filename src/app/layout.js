9acce18363de8f8fa60429cde888445d3649b152
Ryan Kim <rck2001@hotmail.com>
2024-06-25 12:49:18 -0400

    complete analytics component

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <A
+      <Analytics />
       <body className={montserrat.className}>{children}</body>
     </html>
   );
